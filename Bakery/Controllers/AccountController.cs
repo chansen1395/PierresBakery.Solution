0d8f5c2e27ec5279470117d2e3c169ca5f4ab80c
Connor Hansen <chansen13@georgefox.edu>
2021-10-22 08:47:40 -0700

    successfully build db & migrations.

---
+++ Bakery/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Identity;
+// using Bakery.Models;
+// using System.Threading.Tasks;
+// using Bakery.ViewModels;
+
+// namespace Bakery.Controllers
+// {
+//   public class AccountController : Controller
+//   {
+//     private readonly BakeryContext _db;
+//     private readonly UserManager<ApplicationUser> _userManager;
+//     private readonly SignInManager<ApplicationUser> _signInManager;
+
+//     public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BakeryContext db)
+//     {
+//       _userManager = userManager;
+//       _signInManager = signInManager;
+//       _db = db;
+//     }
+
+//     // public ActionResult Index()
+//     // {
+//     //   return View();
+//     // }
+//   }
+// }
