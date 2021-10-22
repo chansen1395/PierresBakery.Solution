042484c6a42d11a6d7201b38398a20224f8f9e0f
Connor Hansen <chansen13@georgefox.edu>
2021-10-22 09:49:22 -0700

    test view-all.

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
 
 namespace Bakery.Controllers
 {
@@ -9,6 +10,14 @@ namespace Bakery.Controllers
       public ActionResult Index()
       {
         return View();
+      }
+
+      public ActionResult ViewAll()
+      {
+        dynamic sampleObject = new ExpandoObject();
+        sampleObject.test1 = "hello";
+        sampleObject.test2 = "there";
+        return View(sampleObject);
       }
 
     }
