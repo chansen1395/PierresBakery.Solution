78637b11121092e56df049930cb0fa6e558b5c2f
Connor Hansen <chansen13@georgefox.edu>
2021-10-24 12:18:39 -0700

    correct minor inconsistencies, clean up code.

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -9,41 +9,35 @@ using System.Collections.Generic;
 
 namespace Bakery.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly BakeryContext _db;
+    public HomeController(BakeryContext db)
     {
-      private readonly BakeryContext _db;
-      public HomeController(BakeryContext db)
-      {
-        _db = db;
-      }
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
-      public ActionResult ViewAll()
-      {
-        dynamic modelAll = new ExpandoObject();
-        modelAll.Flavors = _db.Flavors.ToList();
-        modelAll.Treats = _db.Treats.ToList();
-        return View(modelAll);
-      }
-
-      public ActionResult Search(string searchString)
-      {
-        dynamic searchAll = new ExpandoObject();
-        searchAll.Flavors = _db.Flavors.Where(flavor => flavor.FlavorName.Contains(searchString)).ToList();
-        searchAll.Treats = _db.Treats.Where(treat => treat.TreatName.Contains(searchString)).ToList();
-       
-        return View(searchAll);
-
-        // List<Flavor> model = 
-        //   _db.Flavors.Where(flavor => flavor.FlavorName.Contains(searchString)).ToList();
-
-        // return View(model);
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult ViewAll()
+    {
+      dynamic modelAll = new ExpandoObject();
+      modelAll.Flavors = _db.Flavors.ToList();
+      modelAll.Treats = _db.Treats.ToList();
+      return View(modelAll);
+    }
+
+    public ActionResult Search(string searchString)
+    {
+      dynamic searchAll = new ExpandoObject();
+      searchAll.Flavors = _db.Flavors.Where(flavor => flavor.FlavorName.Contains(searchString)).ToList();
+      searchAll.Treats = _db.Treats.Where(treat => treat.TreatName.Contains(searchString)).ToList();
+      
+      return View(searchAll);
+    }
+  }
 }
