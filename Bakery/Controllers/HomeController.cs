cd983d72f9040836f5f7b548b3630b79884a7e36
Connor Hansen <chansen13@georgefox.edu>
2021-10-24 11:55:54 -0700

    add search function.

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Dynamic;
 using Bakery.Models;
 using System.Threading.Tasks;
 using System.Linq;
+using System;
+using System.Collections.Generic;
 
 
 namespace Bakery.Controllers
@@ -27,6 +29,20 @@ namespace Bakery.Controllers
         modelAll.Flavors = _db.Flavors.ToList();
         modelAll.Treats = _db.Treats.ToList();
         return View(modelAll);
+      }
+
+      public ActionResult Search(string searchString)
+      {
+        dynamic searchAll = new ExpandoObject();
+        searchAll.Flavors = _db.Flavors.Where(flavor => flavor.FlavorName.Contains(searchString)).ToList();
+        searchAll.Treats = _db.Treats.Where(treat => treat.TreatName.Contains(searchString)).ToList();
+       
+        return View(searchAll);
+
+        // List<Flavor> model = 
+        //   _db.Flavors.Where(flavor => flavor.FlavorName.Contains(searchString)).ToList();
+
+        // return View(model);
       }
 
     }
